a37ea8aea854741624826b2c4ef206dc422bde39
RondonLeonR <36230656+RondonLeonR@users.noreply.github.com>
2018-02-08 12:27:51 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,13 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad
 
+edad = parseInt(document.getElementById("edad").value);
 
+    if(edad >=13 && edad <=17)
+    {
+        alert("Eres un adolecente");
+    }
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,17 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad
 
+edad = parseInt(document.getElementById("edad").value);
+//Manera numero 1 de hacer.
+if(edad <13 || edad >17)
+    {
+        alert("No eres un adolecente");
+    }
 
 }//FIN DE LA FUNCIÓN
+
+
+// La manera num 2 de hacer.
+//if(!(edad >=13 && edad <=17));
