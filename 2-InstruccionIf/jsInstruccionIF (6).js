8da08e16346ca237f51aa35001f907f2e3d7aaa1
RondonLeonR <36230656+RondonLeonR@users.noreply.github.com>
2018-02-08 23:35:43 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,22 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad
 
+    edad = parseInt(document.getElementById("edad").value);
 
+    if( edad <= 13)
+        {
+            alert("Usted es un Niño");
+        }
+    else if( edad >=13 && edad <=17 )
+        {
+            alert("Usted es un Adolesente");
+        }
+    else if( edad > 17 )
+        {
+            alert("Usted es un Adulto");
+        }
 
 
 }//FIN DE LA FUNCIÓN
