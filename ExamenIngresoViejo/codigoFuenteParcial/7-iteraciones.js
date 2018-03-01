928849da1953d7df7e75e44089eefcbe5501705a
RondonLeonR <36230656+RondonLeonR@users.noreply.github.com>
2018-03-01 12:12:21 -0300

    Add files via upload

--- ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
@@ -2,7 +2,7 @@
 function Mostrar()
 {
 	var nota;
-    var notaTotal;
+    var notaTotal = 0;
     var sexo;
     var promedio;
     var notaBaja;
@@ -14,27 +14,43 @@ function Mostrar()
         nota=parseInt(prompt("Ingrese Nota: "));
         sexo=prompt("Ingrese sexo('f' o 'm'): ");
 
-        while(!(sexo='f')||(sexo='m'))
+        while(!(sexo=='f'||sexo=='m'))
         {
             sexo=prompt("Error. Reingresar sexo('f' o 'm'): ");
         }
-        if(sexo=='f'||sexo=='F')
-        {
-            sexo= "Femenino"
-        }
-        else 
-        {
-            sexo = "Masculino"
-        }
+            if(sexo=='f'||sexo=='F')
+            {
+                sexo= "Femenino"
+            }
+            else 
+            {
+                sexo = "Masculino"
+            }
 
         while(!(nota>=0 && nota<=10))
         {
             nota=parseInt("Error. Reingrese Nota(entre 0 y 10): ")
         }
 
+        if(nota < notaBaja||flag==0)
+        {
+            notaBaja=nota;
+            flag=1;
+        }
+
+        if(sexo=="Masculino"&&nota>=6)
+        {
+            cantVarApr++;
+        }
+
         notaTotal = notaTotal + nota;
 
-    }
+     }
 
+        promedio = notaTotal/6;
+
+        alert("El promedio de todas las notas es: " +promedio);
+        alert("La nota mas baja es: " +notaBaja);
+        alert(cantVarApr+" fue la cantidad de varones que aprobaron con 6 o mas");
 }
 
--- ExamenIngresoViejo/codigoFuenteParcial/8-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/8-iteraciones.js
@@ -1,6 +1,55 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	
+    var num;
+    var cantPar = 0;
+    var cantNum = 0;
+    var numMax;
+    var numMin;
+    var flag = 0;
+    var resp = 'si';
+    var repet=0;
+    
+while(resp=='si')
+{
+    num = parseInt(prompt("Ingrese numero: "));
+
+    while(!(num  >= 0))
+    {
+        num = parseInt(prompt("Error. Reingrese numero positivo: "));
+    }
+
+    if(num < numMin||flag==0)
+    {
+        numMin=num;
+    }
+    else if(num <numMax||flag==0)
+    {
+        numMax=num;
+        flag=1;
+    }
+       
+    if(num/2==0)
+    {
+        cantPar++;
+    }
+
+    cantNum = cantNum + num;
+
+    res = prompt("Desea Continuar?: ('si' o 'no')");
+
+    if (resp=='si')
+    {
+        repet++;
+    }
+    else if(resp)
+}
+promedio = cantNum/ repet;
+
+document.write("La cantidad de numeros pares es: " +cantPar+ "<br>");
+document.write("El promedio de todos los numeros ingresados es: " +promedio+ "<br>");
+document.write("La suma de todos los numeros es: " +cantNum+ "<br>");
+document.write("El numero minimo fue " +numMin+ " y el numero maximo fue " +numMax);
+
 }
 
